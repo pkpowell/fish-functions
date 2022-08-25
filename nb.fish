2133d6cc9fd7c90b8f92824e0f19caf68a254e4d
Philip Powell <pkpowell@users.noreply.github.com>
2022-08-25 18:29:56 +0200

    Update nb.fish
    check for user

--- nb.fish
+++ nb.fish
@@ -1,3 +1,9 @@
 function nb
-    sudo netbird ssh $argv
+    set args (string split @ $argv)
+    if count args = 1; 
+        echo "user: "|read user
+        sudo netbird ssh $user@$args
+    else
+        sudo netbird ssh $argv
+    end
 end
