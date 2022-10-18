17906ad0d5d8afffd02b1381611cdb89d6854cfc
phil <p@pkpowell.net>
2022-10-18 09:47:49 +0200

    update lop

--- lop.fish
+++ lop.fish
@@ -11,8 +11,25 @@ function lop
 				return
 		end
 	end
-	set app (mdfind -name "$appname" -onlyin /Applications) 
-	echo "opening $app[1] with local $locale"
+	set apps (mdfind -name "$appname" -onlyin /Applications) 
+	# echo "opening $app[1] with local $locale"
+
+	for idx in (seq (count $apps))
+		echo -n "($idx)"
+		echo $apps[$idx] 
+	end
+	echo "(q) Quit"
+	read -p 'echo "Choose your app 1*-"'(count $apps)": " choice
+	
+	switch $choice
+		case q
+			return
+		case "*"
+			if string match --quiet --regex '[a-z]' $choice >/dev/null 
+				return
+			end
+			set app $apps[$choice]
+	end
 
 	open "$app[1]" --args -AppleLanguages "($locale)"
 end
