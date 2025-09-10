470e6570304da4682eeb887350ec20bdb767f2c4
phil <p@pkpowell.net>
2025-09-10 16:03:28 +0200

    updates

--- tp.fish
+++ tp.fish
@@ -1,7 +1,7 @@
 function tp
     set args (string split @ $argv)
     if count args = 1; 
-        set pid (pgrep zt-client)
+        set pid (pgrep args)
         top -pid $pid
     else
         echo "Wrong arg count"
