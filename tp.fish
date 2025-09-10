a2bc34bf2a4c7f33f42b99d0a996757e23b66d09
phil <p@pkpowell.net>
2025-09-10 16:42:16 +0200

    updates

--- tp.fish
+++ tp.fish
@@ -6,7 +6,7 @@ function tp
             echo $pid[$i] 
         end
         echo "(q) Quit"
-        read -p 'echo "Choose your pid 1*-"'(count $pid)": " choice
+        read -p 'echo "Choose your pid 1-"'(count $pid)": " choice
         
         switch $choice
             case q
@@ -19,7 +19,7 @@ function tp
         end
     end
         
-
+    set pid string split ' ' $pid
 
     if [ $pid -gt 1 ] 
         top -pid $pid
