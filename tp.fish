cea0c9163bd9c6b358c8f0f72ffe395071f56268
phil <p@pkpowell.net>
2025-09-10 16:29:34 +0200

    updates

--- tp.fish
+++ tp.fish
@@ -1,5 +1,26 @@
 function tp
     set pid (pgrep $argv)
+    if count(pid) -gt 1 
+        for i in (seq (count $ips))
+            echo -n "($i)"
+            echo $ips[$i] 
+        end
+        echo "(q) Quit"
+        read -p 'echo "Choose your pid 1*-"'(count $pid)": " choice
+        
+        switch $choice
+            case q
+                return
+            case "*"
+                if string match --quiet --regex '[a-z]' $choice >/dev/null 
+                    return
+                end
+                set pid $pid[$choice]
+        end
+    end
+        
+
+
     if [ $pid -gt 1 ] 
         top -pid $pid
     else
